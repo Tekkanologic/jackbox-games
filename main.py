713b35a26305e4ed082fa4cda26ee07329f68366
lukevc <lukevincentcannon@hotmail.co.uk>
2023-07-31 00:51:08 +0100

    Output now shows pack number

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
+"""
+    Entry point for application
+"""
+
 import os
 import random
 import customtkinter
 from tkinter import BitmapImage
 from games import Games
 
-customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         try:
-            from ctypes import windll  # Only exists on Windows.
-
+            from ctypes import windll
             myappid = "mycompany.myproduct.subproduct.version"
             windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
         except ImportError:
             pass
 
-        #self.iconbitmap("icon.ico")
+        self.iconbitmap(r"D:\DEV\UTILS\jackbox games\icon.ico")
 
         # Config window
         self.title("Jackbox games")
         self.geometry("500x300")
 
         # Create input
-        self.grid_columnconfigure((0,1), weight=1)
-        self.grid_rowconfigure((0,1), weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure((0,2), weight=1)
 
         # Player entry
         self.entry = customtkinter.CTkEntry(self, placeholder_text="Number of Players")
-        self.entry.grid(row=0, column=0, padx=10, pady=10, sticky="we")
+        self.entry.grid(row=0, column=0, padx=10, pady=10, sticky="")
 
         # Button
         self.button = customtkinter.CTkButton(self, text="Choose game", command=self.show_game)
-        self.button.grid(row=1, column=0, padx=10, pady=10, sticky="we")
+        self.button.grid(row=1, column=0, padx=10, pady=10, sticky="")
 
         # Output
         self.output = customtkinter.CTkLabel(self, text=" NONE ")
-        self.output.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
-
-        # Update
-        self.mainloop()
+        self.output.grid(row=2, column=0, padx=10, pady=10, sticky="")
 
     def show_game(self):
         numPlayers = int(self.entry.get())
         game = self.ChooseGame(numPlayers)
-        game_name = game[0]
+        game_name = game[0] + " " + "(" + game[3] + ")"
         self.output.configure(text=f"{game_name}")
 
 
@@ -73,4 +73,5 @@ class App(customtkinter.CTk):
 
 if __name__ == "__main__":
     app = App()
+    app.mainloop()
     
